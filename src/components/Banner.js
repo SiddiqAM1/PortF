09cf544bc575e817fd8cda2da3886d04d952ac06
Siddiq <ahmedmuhammadsiddiq@gmail.com>
2023-08-06 14:41:40 +0500

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Siddiq`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer","Full-Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p> I'm a passionate Full-Stack Developer with a strong affinity for crafting innovative web applications. My coding journey started with C# for game development and Python, but it was the dynamic world of web development with JavaScript that truly captivated me. 
+    <br /><br />
+    With hands-on experience in a range of technologies including HTML, CSS, ReactJS, JavaScript, Material-UI, Chakra-UI, and more, I have honed my skills to deliver seamless user experiences. I am well-versed in both front-end and back-end development, making me an ideal candidate to bridge the gap between design and functionality.
+    <br /><br />
+    Leveraging tools like Next.js, Redux, and TypeScript, I have built responsive, data-driven applications that showcase my ability to bring ideas to life. My problem-solving mindset, coupled with a strong attention to detail, allows me to create elegant solutions that meet project goals effectively.
+    <br /><br />
+    I am deeply committed to continuous learning, staying updated with the latest industry trends, and contributing to open-source projects. I'm excited to bring my dedication, creativity, and passion for coding to your team.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
