0ff88bde710af6e5f486b72598b2a3bca9b45e41
Muhammad Siddiq <ahmedmuhammadsiddiq4@gmail.com>
2023-08-08 16:49:13 +0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/ebay.jpg";
+import projImg2 from "../assets/img/threads.threads.webp";
+import projImg3 from "../assets/img/car.png";
+import projImg4 from "../assets/img/crypto.png";
+import projImg5 from "../assets/img/port.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -28,17 +30,12 @@ export const Projects = () => {
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrl: projImg5,
     },
   ];
 
